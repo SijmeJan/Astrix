76a43238dcf68836dd35355933c281fd602714e3
SijmeJan <s.j.paardekooper@qmul.ac.uk>
2017-01-09 15:12:52 +0000

    update writing profiling filses

--- src/astrix/Common/profile.cpp
+++ src/astrix/Common/profile.cpp
@@ -14,37 +14,39 @@ namespace astrix {
 void WriteProfileFile(const char *fileName, int nElement,
 		      float elapsedTime, int cudaFlag)
 {
-  int maxElement = 0;
-  float maxTime = 0.0;
   int maxElementCPU = 0;
   float maxTimeCPU = 0.0;
-  int GPUonlyFlag = 0;
+  int maxElementGPU = 0;
+  float maxTimeGPU = 0.0;
   
   // Read current file if it exists
   std::ifstream infile;
   infile.open(fileName);
   if (infile.is_open()) {
     // Get current number of elements and elapsed time
-    infile >> maxElement >> maxTime;
-    // Save for later
-    maxElementCPU = maxElement;
-    maxTimeCPU = maxTime;
-    // If using device, we need the next line if it exists
-    if (cudaFlag == 1)
-      if (!(infile >> maxElement >> maxTime))
-	GPUonlyFlag = 1;
+    infile >> maxElementCPU >> maxTimeCPU;
+    infile >> maxElementGPU >> maxTimeGPU;
   }
   infile.close();
-  
-  if (maxElement < nElement) {
-    std::ofstream outfile;
-    outfile.open(fileName);
 
-    if (cudaFlag == 1 && GPUonlyFlag == 0)
-      outfile << maxElementCPU << " " << maxTimeCPU << std::endl;
-    outfile << nElement << " " << elapsedTime << std::endl;
-    outfile.close();
+  if (cudaFlag == 1) {
+    if (maxElementGPU < nElement) {
+      maxElementGPU = nElement;
+      maxTimeGPU = elapsedTime;
+    }
+  } else {
+    if (maxElementCPU < nElement) {
+      maxElementCPU = nElement;
+      maxTimeCPU = elapsedTime;
+    }
   }
+  
+  std::ofstream outfile;
+  outfile.open(fileName);
+
+  outfile << maxElementCPU << " " << maxTimeCPU << std::endl; 
+  outfile << maxElementGPU << " " << maxTimeGPU << std::endl;
+  outfile.close();
 }
 
 }
