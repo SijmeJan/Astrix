18a8af6cbab93883fadcae6bbe10c4bef2a7dd68
SijmeJan <s.j.paardekooper@qmul.ac.uk>
2017-01-24 14:03:32 +0000

    ASTRIX_DOUBLE in Makefile

--- src/astrix/Common/definitions.h
+++ src/astrix/Common/definitions.h
@@ -16,7 +16,7 @@ along with Astrix.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef ASTRIX_DEFINITIONS_H
 #define ASTRIX_DEFINITIONS_H
 
-#define USE_DOUBLE -1
+//#define USE_DOUBLE -1
 #define N_EQUATION 4
 
 /*! \namespace astrix
