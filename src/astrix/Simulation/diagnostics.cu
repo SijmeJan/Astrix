35c59fb530cdae611babe8c2e59c9dbd457f9d44
SijmeJan <s.j.paardekooper@qmul.ac.uk>
2017-04-11 10:16:26 +0100

    Sedov + thermal energy

--- src/astrix/Simulation/diagnostics.cu
+++ src/astrix/Simulation/diagnostics.cu
@@ -112,12 +112,89 @@ real2 Simulation::KineticEnergy()
 //##############################################################################
 
 __host__ __device__
+void ThermalEnergySingle(unsigned int i, const real *pVarea,
+                         real4 *pState, real *E)
+{
+  real half = (real) 0.5;
+
+  real d = pState[i].x;
+  real m = pState[i].y;
+  real n = pState[i].z;
+  real e = pState[i].w;
+
+  E[i] = pVarea[i]*(e - half*(m*m + n*n)/d);
+}
+
+//######################################################################
+//######################################################################
+
+__global__ void
+devThermalEnergy(unsigned int nVertex, const real *pVarea,
+                 realNeq *pState, real *E)
+{
+  // n = vertex number
+  unsigned int n = blockIdx.x*blockDim.x + threadIdx.x;
+
+  while (n < nVertex) {
+    ThermalEnergySingle(n, pVarea, pState, E);
+
+    n += blockDim.x*gridDim.x;
+  }
+}
+
+//######################################################################
+//######################################################################
+
+real Simulation::ThermalEnergy()
+{
+  unsigned int nVertex = mesh->GetNVertex();
+
+  realNeq *pState = vertexState->GetPointer();
+
+  const real *pVarea = mesh->VertexAreaData();
+
+  Array<real> *E = new Array<real>(1, cudaFlag, nVertex);
+  real *pE = E->GetPointer();
+
+  if (cudaFlag == 1) {
+    int nBlocks = 128;
+    int nThreads = 128;
+
+    // Base nThreads and nBlocks on maximum occupancy
+    cudaOccupancyMaxPotentialBlockSize(&nBlocks, &nThreads,
+                                       devKineticEnergy,
+                                       (size_t) 0, 0);
+
+    devThermalEnergy<<<nBlocks, nThreads>>>
+      (nVertex, pVarea, pState, pE);
+
+    gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
+  } else {
+    for (unsigned int n = 0; n < nVertex; n++)
+      ThermalEnergySingle(n, pVarea, pState, pE);
+  }
+
+  real e = E->Sum();
+
+  delete E;
+
+  return e;
+}
+
+//##############################################################################
+//##############################################################################
+
+__host__ __device__
 void DensityErrorSingle(unsigned int i, const real *pVarea,
+                        const real2 *pVc,
                         real4 *pState, real4 *pStateOld, real *E)
 {
   real d = pState[i].x;
   real d0 = pStateOld[i].x;
 
+  //E[i] = 0.0;
+  //if (std::abs(pVc[i].x) > 0.35)
   E[i] = pVarea[i]*std::abs(d - d0);
 }
 
@@ -126,13 +203,14 @@ void DensityErrorSingle(unsigned int i, const real *pVarea,
 
 __global__ void
 devDensityError(unsigned int nVertex, const real *pVarea,
+                const real2 *pVc,
                 realNeq *pState, realNeq *pStateOld, real *E)
 {
   // n = vertex number
   unsigned int n = blockIdx.x*blockDim.x + threadIdx.x;
 
   while (n < nVertex) {
-    DensityErrorSingle(n, pVarea, pState, pStateOld, E);
+    DensityErrorSingle(n, pVarea, pVc, pState, pStateOld, E);
 
     n += blockDim.x*gridDim.x;
   }
@@ -143,9 +221,17 @@ devDensityError(unsigned int nVertex, const real *pVarea,
 
 real Simulation::DensityError()
 {
+  real2 Ekin = KineticEnergy();
+  real Eth = ThermalEnergy();
+  real Etot = Ekin.x + Ekin.y + Eth;
+
   vertexStateOld->SetEqual(vertexState);
+
   // Assume this gets the correct solution into vertexState
   SetInitial(simulationTime);
+
+  if (simulationParameter->problemDef == PROBLEM_SEDOV)
+    SedovSetAnalytic(vertexState, Etot);
 
   unsigned int nVertex = mesh->GetNVertex();
 
@@ -153,6 +239,7 @@ real Simulation::DensityError()
   realNeq *pStateOld = vertexStateOld->GetPointer();
 
   const real *pVarea = mesh->VertexAreaData();
+  const real2 *pVc = mesh->VertexCoordinatesData();
 
   Array<real> *E = new Array<real>(1, cudaFlag, nVertex);
   real *pE = E->GetPointer();
@@ -167,16 +254,18 @@ real Simulation::DensityError()
                                        (size_t) 0, 0);
 
     devDensityError<<<nBlocks, nThreads>>>
-      (nVertex, pVarea, pState, pStateOld, pE);
+      (nVertex, pVarea, pVc, pState, pStateOld, pE);
 
     gpuErrchk( cudaPeekAtLastError() );
     gpuErrchk( cudaDeviceSynchronize() );
   } else {
     for (unsigned int n = 0; n < nVertex; n++)
-      DensityErrorSingle(n, pVarea, pState, pStateOld, pE);
+      DensityErrorSingle(n, pVarea, pVc, pState, pStateOld, pE);
   }
 
-  real e = E->Sum();
+  real e = E->Sum()/mesh->GetTotalArea();
+  //if (simulationParameter->problemDef == PROBLEM_LINEAR)
+  //e = e/mesh->GetPy();
 
   delete E;
 
