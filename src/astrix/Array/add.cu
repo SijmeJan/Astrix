b26ac5a045483f6e6421122f8aea33930f8fe164
SijmeJan <s.j.paardekooper@qmul.ac.uk>
2018-11-27 21:37:01 +0000

    Add MultiplyValue

--- src/astrix/Array/add.cu
+++ src/astrix/Array/add.cu
@@ -68,6 +68,51 @@ void Array<T>::AddValue(T value, unsigned int startIndex,
 }
 
 //###################################################
+//! Multiply on device
+//###################################################
+
+template<class T>
+__global__ void
+devMultValue(unsigned int startIndex, unsigned int endIndex,
+            T *data, T value)
+{
+  int i = blockIdx.x*blockDim.x + threadIdx.x + startIndex;
+
+  while (i < endIndex) {
+    data[i] *= value;
+
+    i += gridDim.x*blockDim.x;
+  }
+}
+
+//###################################################
+// Multiply range of array by value
+//###################################################
+
+template <class T>
+void Array<T>::MultiplyValue(T value, unsigned int startIndex,
+                             unsigned int endIndex)
+{
+  T *data = GetPointer();
+
+  if (cudaFlag == 1) {
+    int nThreads = 128;
+    int nBlocks = 128;
+
+    // Base nThreads and nBlocks on maximum occupancy
+    cudaOccupancyMaxPotentialBlockSize(&nBlocks, &nThreads,
+                                       devMultValue<T>,
+                                       (size_t) 0, 0);
+
+    devMultValue<<<nBlocks, nThreads>>>(startIndex, endIndex, deviceVec, value);
+    gpuErrchk(cudaPeekAtLastError());
+    gpuErrchk(cudaDeviceSynchronize());
+  } else {
+    for (unsigned int i = startIndex; i < endIndex; i++) data[i] *= value;
+  }
+}
+
+//###################################################
 // Instantiate
 //###################################################
 
@@ -77,5 +122,12 @@ template void Array<int>::AddValue(int value,
 template void Array<unsigned int>::AddValue(unsigned int value,
                                             unsigned int startIndex,
                                             unsigned int endIndex);
+
+template void Array<int>::MultiplyValue(int value,
+                                        unsigned int startIndex,
+                                        unsigned int endIndex);
+template void Array<unsigned int>::MultiplyValue(unsigned int value,
+                                                 unsigned int startIndex,
+                                                 unsigned int endIndex);
 
 }  // namespace astrix
