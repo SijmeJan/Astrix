b703c368fa3fb18eef6ac10b7dd4a6deb6770bef
SijmeJan <s.j.paardekooper@qmul.ac.uk>
2018-12-04 12:35:14 +0000

    Include cassert

--- src/astrix/Mesh/Refine/insert.cu
+++ src/astrix/Mesh/Refine/insert.cu
@@ -14,6 +14,7 @@ Astrix is distributed in the hope that it will be useful, but WITHOUT ANY WARRAN
 You should have received a copy of the GNU General Public License
 along with Astrix.  If not, see <http://www.gnu.org/licenses/>.*/
 #include <iostream>
+#include <cassert>
 
 #include "../../Common/definitions.h"
 #include "../../Array/array.h"
