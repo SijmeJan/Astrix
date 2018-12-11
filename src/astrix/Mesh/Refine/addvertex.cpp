6ac6d76180cbc5a4ddbd077f3b5a529b119179b7
SijmeJan <s.j.paardekooper@qmul.ac.uk>
2018-12-11 22:13:10 +0000

    Add save in debug mode

--- src/astrix/Mesh/Refine/addvertex.cpp
+++ src/astrix/Mesh/Refine/addvertex.cpp
@@ -125,6 +125,8 @@ int Refine::AddVertices(Connectivity * const connectivity,
 
   // Check if Mesh is valid
   if (debugLevel > 0) {
+    connectivity->Save(900);
+
     try {
       connectivity->CheckEdgeTriangles();
       connectivity->CheckTriangleAreas(predicates, meshParameter);
